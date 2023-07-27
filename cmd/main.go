b97f24a1b945cb75d7b24d2131406a5b934a960c
ggsmith842 <grant.oferrell.smith@gmail.com>
2023-07-27 04:45:55 +0000

    csv to csv with filter example added

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	src "goetl/pkg/sources"
+	tgt "goetl/pkg/targets"
+	tr "goetl/pkg/transformations"
 )
 
 func main() {
 
 	fmt.Println("Hi go-etl!\nEasy pipelines to move data from source to target!")
 
-	src.CsvSrc("data/MORTGAGE30US.csv")
+	data := src.CsvSrc("data/MORTGAGE30US.csv")
+
+	//Filter condiditon is only changed in the function currently
+	//working on way to pass conditions from main
+	highRateData := tr.Filter(data)
+
+	tgt.CsvTgt("HIGHRATEDATA", highRateData)
+
 }
